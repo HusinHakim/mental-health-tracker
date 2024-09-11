84c2a5b6a158b36d9f5124fa3816b671de07879a
HusinHakim <husinhidayatul@gmail.com>
2024-09-11 16:44:03 +0700

    update

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main'
 ]
 
 MIDDLEWARE = [
@@ -53,8 +54,9 @@ ROOT_URLCONF = 'mental_health_tracker.urls'
 
 TEMPLATES = [
     {
+        
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://husin-hidayatul-mentalhealthtracker.pbp.cs.ui.ac.id/", "https://husin-hidayatul-mentalhealthtracker.pbp.cs.ui.ac.id/"]
